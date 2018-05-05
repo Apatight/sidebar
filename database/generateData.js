f54e7b39fe4112eb51090fb90a41cc8849bd5810
jesrah <jesrah@gmail.com>
2018-05-04 18:26:17 -0700

    Generated 10 million records with writeFileStream :)

--- database/generateData.js
+++ database/generateData.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const faker = require('faker');
 
-const results = [];
+const place = {};
 faker.locale = 'en_US';
+//count down from 10m
+const total = 10000000;
 
-for (let i = 0; i < 100000; i += 1) {
-  faker.seed(i);
-
-  const place = {};
+const createSidebars = (i) => {
+	if (i === 1) {
+		console.log('record', i, 'created');
+	};
   place.id = i;
   place.name = faker.commerce.productName();
   place.menu_url = 'http://google.com';
@@ -24,13 +26,39 @@ for (let i = 0; i < 100000; i += 1) {
     'Saturday: 11:30 AM – 9:30 PM',
     'Sunday: 11:30 AM – 9:30 PM',
   ];
-  place.coords = {
-  	lat: null,
-  	lng: null,
-  };
+  place.coords = { lat: null, lng: null };
   place.coords.lat = faker.address.latitude();
   place.coords.lng = faker.address.longitude();
-  results.push(place);
-}
+  return place;
+};
 
-fs.writeFile('./zagatData.json', JSON.stringify(results));
+const generate = (writer, encoding, callback) => {
+  let x = total;
+  function write() {
+    let written = true;
+    do {
+      x -= 1;
+      let stringified = JSON.stringify(createSidebars(x));
+      if (x === total - 1) {
+        stringified = `[${stringified}`;
+      }
+      if (x !== 0) {
+        stringified += ',';
+      }
+      if (x === 0) {
+        stringified += ']';
+        writer.write(stringified, encoding, callback);
+      } else {
+        written = writer.write(stringified, encoding);
+      }
+    } while (x > 0 && written);
+    if (x > 0) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+};
+
+generate(fs.createWriteStream('./zagatData.json', 'utf8', () => {
+  console.log('WriteStream 10million completed!');
+}));

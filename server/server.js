f78f219f37ed558297306fbcfa28b5e72f1f1b4b
jesrah <jesrah@gmail.com>
2018-05-17 16:23:14 -0700

    Added redis-server and redis-cli for cacheing

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-require('newrelic');
-
+const newrelic = require('newrelic');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -8,6 +7,10 @@ const app = express();
 const port = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
 const db = require('../database/index.js');
+
+const redis = require('redis');
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const client = redis.createClient();
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,18 +24,54 @@ app.use('/restaurants', express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/restaurants/:id', function getFile (req, res) {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-})
+});
 
 app.get('/api/restaurants/:id', function getRestaurant (req, res) {
   let id = req.params.id;
-  db.findOne(id)
-  .then((data) => {
-    res.send(data);
-  })
-  .catch((err) => {
-    console.log('ERROR: ', err);
-  });
+  client.get(id, (err, restaurant) => {
+    if (restaurant) {
+      res.send(restaurant);
+    }
+    else {
+      db.findOne(id)
+      .then((data) => {
+        client.setex(id, 3600, JSON.stringify(data))
+        res.send(data);
+      })
+      .catch((err) => {
+        console.log('ERROR: ', err);
+      });
+    }
+  }); 
 });
+
+//Redis
+
+// const getCache = (req, res) => {
+//   let id = req.params.id;
+//   client.get(id, (err, restaurant) => {
+//     if (restaurant) {
+//       res.send(restaurant);
+//     } else {
+//         app.get('/api/restaurants/:id', function getRestaurant (req, res) {
+//         let id = req.params.id;
+//         console.log('id is', id);
+//         db.findOne(id)
+//         .then((data) => {
+//           let restaurant = data;
+//           client.setex(id, 3600, JSON.stringify(data));
+
+//           res.send(data);
+//         })
+//         .catch((err) => {
+//           console.log('ERROR: ', err);
+//         });
+//       });
+//     }
+//   });
+// }
+
+// app.get('/api/restaurants/:id', getCache);
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
